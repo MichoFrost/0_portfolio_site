be9cce8994ccf5b2888b1885e1aa25c09007fc8d
MichoFrost <michofrost@gmail.com>
2023-06-10 20:22:39 +0200

    FÆRDIG med JAVASCRIPT!

--- JS/intro.js
+++ JS/intro.js
@@ -25,9 +25,9 @@ function headingIteration(){
 }
 
 // START SIDEN
-startIntro();
+// startIntro();
 function startIntro(){
-  console.log('startIntro');
+  // console.log('startIntro');
   setTimeout(function(){
     welcome.classList.add('visible');
   }, 1000);
@@ -43,15 +43,18 @@ function startIntro(){
   }, 2800)
 }
 
-// startSite();
+startSite();
 // circle.classList.add('visible'); // FJERNES!
 function startSite(){
   expandCircle();
   removeIntroText();
+  // setTimeout(() => {
+  //   headingIteration();
+  // }, 3000);
+
   setTimeout(() => {
-    headingIteration();
-  }, 3000);
-  burgerMenu.classList.add('visible');
+    burgerMenu.classList.add('visible');
+  }, 0); // 6000
 }
 
 // FJERN INTRO-TEKST
@@ -72,7 +75,7 @@ function expandCircle(){
     themeStarContainers.forEach(e => {
       e.classList.add('visible');
     })
-  }, 1400);
+  }, 0); // 1400
 }
 
 // EVENT-LISTENERS
@@ -86,24 +89,109 @@ burgerMenu.addEventListener('click', () => {
   burgerLines.forEach((e) => {
     e.classList.toggle('clicked');
   })
+  closeAllNavbarSections();
 })
 
-
-
 // STJERNE-NAVIGATION
-
-
+const allThemeSections = Array.from(document.querySelectorAll('.theme'));
 const closeIcon = document.querySelector('.closeIcon');
+const star1 = document.querySelector('.star_theme1_container');
+const theme1 = document.querySelector('.theme1');
+const star2 = document.querySelector('.star_theme2_container');
+const theme2 = document.querySelector('.theme2');
+const star3 = document.querySelector('.star_theme3_container');
+const theme3 = document.querySelector('.theme3');
 const star4 = document.querySelector('.star_theme4_container');
 const theme4 = document.querySelector('.theme4');
+const star5 = document.querySelector('.star_theme5_container');
+const theme5 = document.querySelector('.theme5');
 
+star1.addEventListener('click', function(){
+  theme1.classList.add('visible');
+  closeIcon.classList.add('visible');
+  burgerMenu.classList.remove('visible');
+})
+star2.addEventListener('click', function(){
+  theme2.classList.add('visible');
+  closeIcon.classList.add('visible');
+  burgerMenu.classList.remove('visible');
+})
+star3.addEventListener('click', function(){
+  theme3.classList.add('visible');
+  closeIcon.classList.add('visible');
+  burgerMenu.classList.remove('visible');
+})
 star4.addEventListener('click', function(){
   theme4.classList.add('visible');
   closeIcon.classList.add('visible');
-  
+  burgerMenu.classList.remove('visible');
+})
+star5.addEventListener('click', function(){
+  theme5.classList.add('visible');
+  closeIcon.classList.add('visible');
+  burgerMenu.classList.remove('visible');
 })
 
 closeIcon.addEventListener('click', function(){
-  theme4.classList.remove('visible');
+  allThemeSections.forEach((e) => {
+    e.classList.remove('visible');
+  })
+  // theme4.classList.remove('visible');
   closeIcon.classList.remove('visible');
+  burgerMenu.classList.add('visible');
 })
+
+// MODAL
+const modal = document.querySelector('.modal');
+const modalImage = document.querySelector('.modal_image');
+const body = document.querySelector('body');
+
+body.addEventListener('click', function(event){
+  if(!event.target.classList.contains('image')) return;
+  console.log(event.target.tagName);
+  modalImage.src = event.target.src;
+  openModal();
+})
+
+function openModal() {
+  modal.classList.toggle('visible');
+  overlay.classList.toggle('visible');
+}
+
+// CLICK PÅ OVERLAY (gennemsigtige)
+overlay.addEventListener('click', function(){
+  overlay.classList.remove('visible');
+  navbar.classList.remove('visible');
+  modal.classList.remove('visible');
+  burgerLines.forEach((e) => {
+    e.classList.remove('clicked');
+  })
+  closeAllNavbarSections();
+})
+
+// NAVBAR-NAVIGATION
+
+const allNavbarSections = Array.from(document.querySelectorAll('.navbar_section'));
+const allNavbarLinks = Array.from(document.querySelectorAll('.navbar_link'));
+
+allNavbarLinks.forEach(e => {
+  e.addEventListener('click', clickLinks)
+})
+
+function closeAllNavbarSections(){
+  allNavbarSections.forEach(e => {
+    e.classList.remove('visible');
+  })
+}
+
+function clickLinks(event) {
+  if(!event.target.classList.contains('navbar_link')) return;
+  const link = event.target.closest('.navbar_link');
+  const section = allNavbarSections.find(s => s.dataset.menu === link.dataset.link);
+  if(section.classList.contains('visible')) return;
+
+  closeAllNavbarSections();
+  setTimeout(function(){
+    section.classList.toggle('visible');
+  },200)
+}
